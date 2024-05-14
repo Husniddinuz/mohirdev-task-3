b527439159c60b4e7ded38e1c78f9288629d5902
Husniddin Tugizov <husniddindev@gmail.com>
2024-05-15 02:32:31 +0500

    3-task commit

---
+++ task-3/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.Write("Tug'ilgan yilingizni kiriting: ");
+int tugilganYil = System.Convert.ToInt32(System.Console.ReadLine());
+
+int joriyYil = System.DateTime.Now.Year;
+int yosh = joriyYil - tugilganYil;
+int kunlardaYosh = yosh * 365;
+
+System.Console.WriteLine($"Yoshingiz kunlarda: {kunlardaYosh}");
